1de2805f2292b1d0e1bc02d16672256cfd200478
LeeHR <infinitapple@hanmail.net>
2017-03-31 10:26:20 +0900

    add filestream in server

---
+++ TeamProjServer/TeamProjServer/fileStream.h
@@ -0,0 +1,17 @@
+#include <iostram>
+using namespace std;
+
+class fileStream{
+	public:
+	int functionA(int arg);
+	
+	private:
+	
+	
+	
+};
+
+int functionA(int arg){
+	cout<<"This is server filestream. arg:"<<arg<<endl;
+	return 0;
+}
