135e33adc6b409999c51809f9b845688e6df9d4c
YOURIEPI <dltkdals329@naver.com>
2017-03-31 09:58:55 +0900

    0.0 Basic code <server>

---
+++ TeamProjServer/TeamProjServer/Server.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define SOCKETNUM 8888
+using namespace std;
+
+
+int main(void) {
+
+	cout << "hi" << endl;
+	return 0;
+}
---
+++ TeamProjServer/TeamProjServer/Server.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef __SERVER
+#define __SERVER
+
+class GooglerServer {
+
+
+
+	//server
+
+
+
+};
+
+
+#endif // !__SERVER
