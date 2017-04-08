c1cc1bdced31825687939d5db09efbf471bf9d1b
InfinitApple <InfinitApple@hanmail.net>
2017-04-08 19:17:11 +0900

    Server v0.1.0

--- main.cpp
+++ main.cpp
@@ -4,9 +4,7 @@ int main(void) {
 	ServerManager* InsSM;
 	char* fileName;
 
-	cout << "저장될 파일 이름 :";
-	cin >> fileName;
-	InsSM = new ServerManager(fileName);
+	InsSM = new ServerManager();
 	InsSM->FileRecvStart();
 
 	return 0;
